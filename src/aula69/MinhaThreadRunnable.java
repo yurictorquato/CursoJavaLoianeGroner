023722af8d3b4dacfe4aeb8b0bfe84ebd92ef00d
Yuri Cruz Torquato <yurictorquato@gmail.com>
2025-07-27 10:44:08 -0300

    Atualização

--- src/aula69/MinhaThreadRunnable.java
+++ src/aula69/MinhaThreadRunnable.java
@@ -1,4 +1,33 @@
 package aula69;
 
-public class MinhaThreadRunnable {
+public class MinhaThreadRunnable implements Runnable {
+
+    // Atributes
+    private String nome;
+    private int tempo;
+
+    // Constructor
+    public MinhaThreadRunnable(String nome, int tempo) {
+        this.nome = nome;
+        this.tempo = tempo;
+
+        // Thread thread = new Thread();
+        // thread.start();
+    }
+
+    // Methods
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("%s: contador %d".formatted(nome, i));
+            try {
+                Thread.sleep(tempo);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println("%s terminou a execução".formatted(nome));
+    }
 }
